4cb0c930c86a855ed692a93011d24e28c08926a3
Bipul27 <53123431+Bipul27@users.noreply.github.com>
2020-03-29 00:11:36 +0600

    RandomBricks

--- src/Gui/GameController.java
+++ src/Gui/GameController.java
@@ -1,20 +1,19 @@
 
-package Gui;
-import logic.bricks.OBrick;
-
+package gui;
+import logic.SimpleBoard;
+import logic.bricks.ZBrick;
 
 public class GameController {
-    
-    
+    private SimpleBoard board = new SimpleBoard(25,10);
     private final GuiController viewController;
-    private OBrick brick;
     
-    public GameController(GuiController c) {
-        brick =new OBrick();
-		this.viewController = c;
-		//this.viewController.setEventLister(this);
-		//board.createNewBrick();
-		this.viewController.initGameView(brick);
-		//this.viewController.bindScore(board.getScore().scoreProperty());
-	}
+    private ZBrick brick;
+    
+    public GameController(GuiController c)
+    {
+        this.viewController = c;
+        board.createNewBrick();
+        this.viewController.initGameView( board.getBoardMatrix() ,board.getCurrentShape()); 
+        
+    }
 }
