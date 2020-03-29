839c08e2baaa0c8fcdc48d6394b5d4396cb99b47
2018331045 <52996502+2018331045@users.noreply.github.com>
2020-03-30 02:58:08 +0600

    Update SBrick.java

--- src/logic/bricks/SBrick.java
+++ src/logic/bricks/SBrick.java
@@ -3,7 +3,7 @@ package logic.bricks;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SBrick {
+public class SBrick implements Brick {
 	
 private final List<int[][]> brickMatrix = new ArrayList<>();
 	
