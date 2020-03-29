a3bd333a645259be908808b758fddeb56900806e
2018331045 <52996502+2018331045@users.noreply.github.com>
2020-03-30 02:57:05 +0600

    Update LBrick.java

--- src/logic/bricks/LBrick.java
+++ src/logic/bricks/LBrick.java
@@ -3,7 +3,7 @@ package logic.bricks;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LBrick {
+public class LBrick implements Brick {
 	
 private final List<int[][]> brickMatrix = new ArrayList<>();
 	
