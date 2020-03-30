f680cd3ac64e075eb3d5628a2f0c4f55204953e5
2018331045 <52996502+2018331045@users.noreply.github.com>
2020-03-31 02:46:36 +0600

    Adding Viewdata
    Using getter to get position for controller

---
+++ src/logic/ViewData.java
@@ -0,0 +1,35 @@
+package logic;
+
+public class ViewData {
+	
+	//private final int[][] nextBrickData;
+	private final int[][] brickData;
+	private final int xPosition;
+	private final int yPosition;
+	
+	public ViewData(int[][] brickData,
+			int xPosition,
+			int yPosition) {
+		this.brickData = brickData;
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+	}
+	
+	
+	/*public int[][] getNextBrickData() {
+		return nextBrickData;
+	}
+*/
+
+	public int[][] getBrickData() {
+		return brickData;
+	}
+	public int getxPosition() {
+		return xPosition;
+	}
+	public int getyPosition() {
+		return yPosition;
+	}
+	
+	
+}
