e1b99be3c4cbda11b6eb95f0591dd8f5b98b2b9c
2018331045 <52996502+2018331045@users.noreply.github.com>
2020-03-31 02:54:09 +0600

    Update GameController.java
    Changing it according to brick down

--- src/Gui/GameController.java
+++ src/Gui/GameController.java
@@ -1,20 +1,30 @@
 
 package Gui;
-
 import logic.SimpleBoard;
+import logic.ViewData;
+import logic.events.InputEventListener;
 
-public class GameController {
-    private SimpleBoard board = new SimpleBoard(25, 10);
-    private final GuiController viewController;
+public class GameController implements InputEventListener {
     
-    public GameController(GuiController c)
-    {
-
-       this.viewController = c;
-		//this.viewController.setEventLister(this);
+    
+    private SimpleBoard board = new SimpleBoard(25, 10);
+    
+    private final GuiController viewController;
+  
+    
+    public GameController(GuiController c) {
+       // brick =new JBrick();
+		this.viewController = c;
+		this.viewController.setEventLister(this);
 		board.createNewBrick();
-		this.viewController.initGameView(board.getBoardMatrix(),board.getCurrentShape());
-
-        
-    }
+		this.viewController.initGameView(board.getBoardMatrix(),board.getViewData());
+		//this.viewController.bindScore(board.getScore().scoreProperty());
+	}
+    @Override
+    public ViewData onDownEvent() {
+		 board.moveBrickDown();
+		
+		
+		return board.getViewData();
+	}
 }
