26333088d76a0b83701e7bcffef69040981503c6
2018331045 <52996502+2018331045@users.noreply.github.com>
2020-04-02 21:44:05 +0600

    Update RandomBrickGenerator.java

--- src/logic/bricks/RandomBrickGenerator.java
+++ src/logic/bricks/RandomBrickGenerator.java
@@ -29,13 +29,13 @@ public class RandomBrickGenerator {
 	}
 	
 	
-	/* public Brick getBrick() {
+	 public Brick getBrick() {
 		if(nextBricks.size() <= 1) {
 			nextBricks.add(brickList.get(ThreadLocalRandom.current().nextInt(brickList.size())));
 		}
 		return nextBricks.poll();
 	}
-	*/
+	
 	
 	
 	
