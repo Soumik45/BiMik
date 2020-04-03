92d24c21ac2527fbaed1b597967eee38a695880b
2018331045 <52996502+2018331045@users.noreply.github.com>
2020-04-03 18:26:15 +0600

    Update InputEventListener.java

--- src/logic/events/InputEventListener.java
+++ src/logic/events/InputEventListener.java
@@ -5,7 +5,21 @@ import logic.ViewData;
 
 
 public interface InputEventListener {
-    ViewData onDownEvent();
+
+   
+  //  ViewData onDownEvent(MoveEvent event);
+
+    public ViewData onDownEvent(MoveEvent event);
+
+    //ViewData onLeftEvent();
+
+    // ViewData onLeftEvent();
+
+    //public ViewData onLeftEvent(MoveEvent event);
+
+    public ViewData onLeftEvent();
+
+    public ViewData onRightEvent();
     
     
 }
