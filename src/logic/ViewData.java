42a686c301e30e1fc8abcdefdda9bab1e831785f
2018331045 <52996502+2018331045@users.noreply.github.com>
2020-04-04 19:35:05 +0600

    Update ViewData.java

--- src/logic/ViewData.java
+++ src/logic/ViewData.java
@@ -2,24 +2,24 @@ package logic;
 
 public class ViewData {
 	
-	//private final int[][] nextBrickData;
+	private final int[][] nextBrickData;
 	private final int[][] brickData;
 	private final int xPosition;
 	private final int yPosition;
 	
 	public ViewData(int[][] brickData,
 			int xPosition,
-			int yPosition) {
+			int yPosition, int [][]nextBrickData) {
 		this.brickData = brickData;
 		this.xPosition = xPosition;
 		this.yPosition = yPosition;
+                this.nextBrickData= nextBrickData;
 	}
-	
 	
 	public int[][] getNextBrickData() {
 		return nextBrickData;
 	}
-
+	
 
 	public int[][] getBrickData() {
 		return brickData;
