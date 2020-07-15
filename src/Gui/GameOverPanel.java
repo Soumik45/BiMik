167ea3c246c75239972731eeedea410d64731254
2018331045 <52996502+2018331045@users.noreply.github.com>
2020-07-15 20:05:26 +0600

    Added two new java class in GUI

---
+++ src/Gui/GameOverPanel.java
@@ -0,0 +1,18 @@
+
+package Gui;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+public class GameOverPanel extends BorderPane{
+
+	public GameOverPanel() {
+		Label gameOverLabel = new Label("GAME OVER!");
+		gameOverLabel.getStyleClass().add("gameOverStyle");
+		
+		setCenter(gameOverLabel);
+	}
+	
+	
+
+}
