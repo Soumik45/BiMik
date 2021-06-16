439103a97c1a56e4267106b3d818609f94e58f09
2018331045 <52996502+2018331045@users.noreply.github.com>
2021-06-16 17:35:47 +0600

    Update Score.java

--- src/logic/Score.java
+++ src/logic/Score.java
@@ -1,20 +1,38 @@
 
+
 package logic;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
 public class Score {
-	
+	FileDeo ob = new FileDeo();
 	private IntegerProperty score = new SimpleIntegerProperty(0);
-
+        public IntegerProperty soumik=score;
+        int ssoumik=0;
 	public IntegerProperty scoreProperty() {
 		return score;
 	}
 
 	public void add(int i) {
+            int cnt=0;
 		score.setValue(score.getValue() + i);
+               // System.out.println(score);
+                ssoumik = score.getValue().intValue();
+                 ob.get(ssoumik);
 	}
-	
-
+        
+        public int deo() {
+            ob.filekamkaj();
+             System.out.println(ob.highScore);
+            return ob.highScore;
+        }
+        private String path;
+        private String fname;
+      
+     
 }
